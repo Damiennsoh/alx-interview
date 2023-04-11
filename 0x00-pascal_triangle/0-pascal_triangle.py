50a5cfb9c031adf7fb55db77fd6d94f31c1fb5c7
Damiennsoh <psycaro20@gmail.com>
2023-04-11 11:22:00 +0530

    updated the python file within the repository

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,28 @@
+
+#!/usr/bin/python3
+"""
+# Interview question about the Pascal Triangle
+# Returns a list of integers the pascal triangle
+"""
+
+
 def pascal_triangle(n):
+    """
+    This function returns a list of lists of integers.
+    representing pascal's triangle with n.
+    """
+
+    arr = [[] for i in range(n)]
     if n <= 0:
         return []
-    else:
-        triangle = [[1]]
-        for i in range(1, n):
-            row = [1]
-            for j in range(1, i):
-                row.append(triangle[i-1][j-1] + triangle[i-1][j])
-            row.append(1)
-            triangle.append(row)
-        return [num for row in triangle for num in row]
 
-        # Example usage of the pascal_triangle function
-n = int(input("Enter a number: "))
+    for i in range(n):
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                arr[i].append(1)
+            else:
+                arr[i].append(arr[i - 1][j - 1] + arr[i - 1][j])
+    return arr
+n = 5 #int(input("Enter a number: "))
 result = pascal_triangle(n)
 print(f"The Pascal's triangle of level {n} is: {result}")
-

8541f6ce0b615a19096ae38972cfeb1d68d90209
Damien Nsoh Ayine <101427988+Damiennsoh@users.noreply.github.com>
2023-04-11 14:13:33 +0530

    created a function of the island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""
-0-main
-"""
-island_perimeter = __import__('0-island_perimeter').island_perimeter
+'''Island Perimeter:
+    returns the perimeter of the island described in grid'''
 
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
+
+def island_perimeter(grid):
+    '''island perimenter function'''
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
